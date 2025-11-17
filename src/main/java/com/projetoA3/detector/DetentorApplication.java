6aebd21edada9562ab03b8e9cf2b9c3067015110
Eduardo Travassos <eduardotrvassos@gmail.com>
2025-11-17 19:41:38 -0300

    alterações

--- src/main/java/com/projetoA3/detector/DetentorApplication.java
+++ src/main/java/com/projetoA3/detector/DetentorApplication.java
@@ -1,10 +1,17 @@
 package com.projetoA3.detector;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class DetentorApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DetentorApplication.class, args);
